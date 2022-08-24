13342e7d58722f83759092ea4133e993cb0d31fb
RajathShenoy09 <108508428+RajathShenoy09@users.noreply.github.com>
2022-08-24 18:36:07 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
-public class Helloworld {
-  Public static void main(String args[]) {
+public class Helloworld 
+{
+  Public static void main(String args[]) 
+  {
     System.out.println("Hello World");
   }
 }
